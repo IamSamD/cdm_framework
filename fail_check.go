73136b4956026039187777ab89b130c113103510
iamsamd <sam.dickinson@amido.com>
2025-08-28 11:31:25 +0100

    use exit code 2 when check legitimately fails

--- fail_check.go
+++ fail_check.go
@@ -6,5 +6,5 @@ import "os"
 // It simple uses os.Exit(1) to exit the check with an exit code of 1
 // It is included in the cdm_framework in order to ensure that all checks fail with the same behaviour while also providing a more high level experience for check development
 func FailCheck() {
-	os.Exit(1)
+	os.Exit(2)
 }
